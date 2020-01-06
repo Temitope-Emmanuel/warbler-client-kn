ec0c9a1302765fff5bfaa1d68a6d79b1767e2c47
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-01-05 18:13:29 -0800

    v1

---
+++ src/containers/App.js
@@ -0,0 +1,81 @@
+import React,{Component, Fragment} from "react";
+import Navbar from "./Navbar";
+import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
+import { authUser } from "../store/actions/auth";
+import { removeError } from "../store/actions/errors";
+import withAuth from "../hocs/withAuth";
+import MessageForm from "../containers/MessageForm";
+
+class App extends Component{
+  state={}
+
+
+  render(){
+    const {currentUser,errors,authUser,removeError} = this.props
+    return(
+      <div className="onboarding">
+        <Navbar />
+          <Switch>
+        <Route
+          exact
+          path="/"
+          render={props => <Homepage currentUser={currentUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
+                buttonText="Log in"
+                heading="Welcome Back."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                removeError={removeError}
+                errors={errors}
+                onAuth={authUser}
+                signUp
+                buttonText="Sign me up!"
+                heading="Join Warbler today."
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          path="/users/:id/messages/new"
+          component={withAuth(MessageForm)}
+        />
+          </Switch>
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+    errors: state.errors
+  };
+}
+
+
+export default withRouter(
+  connect(mapStateToProps,{authUser,removeError})(App)
+)
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
+import { BrowserRouter as Router } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import { setAuthorizationToken, setCurrentUser } from "./store/actions/auth";
+import jwtDecode from "jwt-decode";
+
+
+
+const store = configureStore()
+if (localStorage.jwtToken) {
+    setAuthorizationToken(localStorage.jwtToken);
+    // prevent someone from manually tampering with the key of jwtToken in localStorage
+    try {
+      store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
+    } catch (e) {
+      store.dispatch(setCurrentUser({}));
+    }
+  }
+  
+
+
+ReactDOM.render(
+<Provider store = {store}>
+    <Router>
+        <App /> 
+    </Router>
+</Provider>, document.getElementById("root"));
+registerServiceWorker();
